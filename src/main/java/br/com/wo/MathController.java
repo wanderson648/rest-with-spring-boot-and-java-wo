df9045f68f9fac0f7803732ea536a3f1fec13ac0
wanderson648 <testepedroca@gmail.com>
2024-03-25 10:46:47 -0300

    implements function request sum for math calc

---
+++ src/main/java/br/com/wo/MathController.java
@@ -0,0 +1,38 @@
+package br.com.wo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    public final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String formatString = strNumber.replaceAll(",", ".");
+        if(isNumeric(formatString)) return Double.parseDouble(formatString);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String formatString = strNumber.replaceAll(",", ".");
+        return formatString.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
