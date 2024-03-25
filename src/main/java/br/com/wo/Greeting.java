9dd08d20dccbf683d1e98c7790e93bbb3c70859d
Wanderson Oliviera da Silva <wanderson@medware.local>
2024-03-25 09:26:30 -0300

    first request with requestParams

---
+++ src/main/java/br/com/wo/Greeting.java
@@ -0,0 +1,19 @@
+package br.com.wo;
+
+public class Greeting {
+    private final long id;
+    private final String name;
+
+    public Greeting(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/br/com/wo/GreetingController.java
@@ -0,0 +1,20 @@
+package br.com.wo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    public static final String template = "Hello, %s!";
+    public final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
